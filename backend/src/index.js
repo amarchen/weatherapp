756655447d945d1913dbc6e5a09ec9919b9ded4d
Artem Marchenko <artem.marchenko@gmail.com>
2018-09-23 01:46:39 +0300

    Fixed backend lints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
-const debug = require('debug')('weathermap');
+/* eslint quotes: ["warn", "double"] comma-dangle: ["warn", "only-multiline"] */
+// const debug = require('debug')('weathermap');
 
-const Koa = require('koa');
-const router = require('koa-router')();
-const fetch = require('node-fetch');
-const cors = require('kcors');
+const Koa = require("koa");
+const router = require("koa-router")();
+const fetch = require("node-fetch");
+const cors = require("kcors");
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || "";
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
@@ -19,28 +20,28 @@ const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 const fetchForecast = async () => {
   const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
-router.get('/api/weather', async ctx => {
+router.get("/api/weather", async ctx => {
   const weatherData = await fetchWeather();
 
-  ctx.type = 'application/json; charset=utf-8';
+  ctx.type = "application/json; charset=utf-8";
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
 });
 
-router.get('/api/forecast', async ctx => {
+router.get("/api/forecast", async ctx => {
   const forecastData = await fetchForecast();
 
-  ctx.type = 'application/json; charset=utf-8';
-  ctx.body = forecastData ? forecastData : {error: "something went wrong, still returning 200"};
+  ctx.type = "application/json; charset=utf-8";
+  ctx.body = forecastData || {error: "something went wrong, still returning 200"};
 });
 
 app.use(router.routes());
